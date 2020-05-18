6fe14b33b62e4af661a2926632d55a55fb82e064
Administrator <Administrator@Admin>
2020-05-18 10:00:55 +0700

    level1

---
+++ main.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Administrator
+ *
+ * Created on April 7, 2020, 9:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    printf("n");
+    
+    return (EXIT_SUCCESS);
+}
+
